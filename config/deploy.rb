23973f85db5748954e1212fe5721d9da20c2514b
collinsugwu1 <collinsuchinaka@gmail.com>
2017-04-19 15:00:25 -0700

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,21 +16,22 @@ set :branch, "developer-dashboard"
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
-set :deploy_to, '/var/www/html/collins/getDev'
+set :deploy_to, '/var/www/html/collins/getdev'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
-
+set :scm, :git
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+set :format, :pretty 
 
 # Default value for :pty is false
 # set :pty, true
-
+set :pty, true
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
-
+set :use_sudo, false
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
